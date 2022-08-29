9ae96d102326b52587d58797c7aaa5fd4f3b40b9
aman <samanjot94@gmail.com>
2022-08-30 00:56:23 +0530

    completd

---
+++ script.js
@@ -0,0 +1,28 @@
+var button=document.querySelector("#btn");
+var input_trans=document.querySelector("#inputtrans");
+var output_trans=document.querySelector("#outputtrans");
+
+var input=input_trans.value;
+ 
+var serverurl="	https://api.funtranslations.com/translate/leetspeak.json"
+
+function getTranslated(text){
+    return serverurl + "?" +"text=" +text
+}
+
+function errorHandler(error){
+    console.log(error);
+}
+//button events
+button.addEventListener("click",function(){
+    fetch(getTranslated(input))
+    .then(response=>response.json())
+    .then(json=>{
+        var translatedtext=json.contents.translated;
+        console.log(translatedtext);
+        output_trans.innerHTML=translatedtext;
+      })
+    .catch(errorHandler)
+    
+});
+
